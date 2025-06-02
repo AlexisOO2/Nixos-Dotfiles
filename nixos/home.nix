8b377337a7f4c87408181b3db612eb33d2f84cdf
Aiden <alexismonroy17@proton.me>
2025-06-02 16:37:32 +0200

    New Version Mon Jun  2 04:37:32 PM CEST 2025

--- nixos/home.nix
+++ nixos/home.nix
@@ -24,8 +24,8 @@
 	  ipc = "on";
 	  splash = false;
   	  splash_offset = 2.0;
-	  preload = ["/Pictures/Wallpapers/animeskull.png"];
-	  wallpaper = ["DP-2,/Pictures/Wallpapers/animeskull.png"];
+	  preload = ["~/Pictures/Wallpapers/animeskull.png"];
+	  wallpaper = ["DP-2,~/Pictures/Wallpapers/animeskull.png"];
 	};
   };
 

e45240d396e48bd650bea83b6d81defda420ea52
Aiden <alexismonroy17@proton.me>
2025-06-02 15:53:36 +0200

    New Version Mon Jun  2 03:53:36 PM CEST 2025

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -149,6 +149,7 @@ nixpkgs.config.permittedInsecurePackages = [
  environment.systemPackages = with pkgs; [
    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
    wget
+   systemd
    git
    alacritty
    btop

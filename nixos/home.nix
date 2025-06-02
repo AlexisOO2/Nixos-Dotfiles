9276a0e372be40d112928b1e08acbec1662583f7
Aiden <alexismonroy17@proton.me>
2025-06-02 19:37:58 +0200

    New Version Mon Jun  2 07:37:58 PM CEST 2025

--- nixos/home.nix
+++ nixos/home.nix
@@ -10,8 +10,28 @@
   home.username = "aiden";
   home.homeDirectory = "/home/aiden";
   home.stateVersion = "25.05"; 
-  
-  home.file.".icons/default".source = "${pkgs.bibata-cursors}/share/icons/bibata-cursors"; 
+
+	home.pointerCursor = 
+	    let 
+	      getFrom = url: hash: name: {
+		  gtk.enable = true;
+		  x11.enable = true;
+		  name = name;
+		  size = 48;
+		  package = 
+		    pkgs.runCommand "moveUp" {} ''
+		      mkdir -p $out/share/icons
+		      ln -s ${pkgs.fetchzip {
+			url = url;
+			hash = hash;
+		      }} $out/share/icons/${name}
+		  '';
+		};
+	    in
+	      getFrom 
+		"https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Classic.tar.xz "
+		"sha256-BvVE9qupMjw7JRqFUj1J0a4ys6kc9fOLBPx2bGaapTk="
+		"bibata-cursors";
 
   #home.pointerCursor = {
   #	name = "bibata-cursors";

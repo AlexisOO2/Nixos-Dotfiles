9b64d9134fa0f648e18fe88f59ffea295b74783e
Aiden <alexismonroy17@proton.me>
2025-06-07 11:30:38 +0200

    New Version Sat Jun  7 11:30:38 AM CEST 2025

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -70,7 +70,7 @@ in
   networking.hostName = "nixos-aiden"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros usthis by default.
 
   # Set your time zone.
   time.timeZone = "Europe/Madrid";

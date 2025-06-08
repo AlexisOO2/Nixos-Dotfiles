185d2b74713b23967abfde13fa38d6649aa98ef2
Aiden <alexismonroy17@proton.me>
2025-06-08 12:00:19 +0200

    New Version Sun Jun  8 12:00:19 PM CEST 2025

--- nixos/yazi/yazi.nix
+++ nixos/yazi/yazi.nix
@@ -1,9 +1,25 @@
+{config,lib,pkgs, ... }:
 {
   programs.yazi = {
   	enable = true;
-
-	#flavors = {
-	  # cat = ./catppuccin-macchiato.yazi;
-	#};
+	plugins = {
+	   git = pkgs.yaziPlugins.git;
+	   sudo = pkgs.yaziPlugins.sudo;
+	   ouch = pkgs.yaziPlugins.ouch;
+	   glow = pkgs.yaziPlugins.glow;
+	   diff = pkgs.yaziPlugins.diff;
+	   rsync = pkgs.yaziPlugins.rsync;
+	   mount = pkgs.yaziPlugins.mount;
+	   duckdb = pkgs.yaziPlugins.duckdb;
+	   yatline = pkgs.yaziPlugins.yatline;
+	   restore = pkgs.yaziPlugins.restore;
+	   starship = pkgs.yaziPlugins.starship;
+	   projects = pkgs.yaziPlugins.projects;
+	   full-border = pkgs.yaziPlugins.full-border;
+	};
+	
+	flavors = {
+	   cat = ./catppuccin-macchiato.yazi;
+	};
   };
 }

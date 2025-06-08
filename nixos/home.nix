de0946d1660fd1e46e7ca8408dc1d459d227a3d7
Aiden <alexismonroy17@proton.me>
2025-06-08 21:45:58 +0200

    New Version Sun Jun  8 09:45:58 PM CEST 2025

--- nixos/home.nix
+++ nixos/home.nix
@@ -38,6 +38,27 @@
   	EDITOR = "nvim";
   };
 
+  services.swaync = {
+    enable = true;
+    settings = {
+	    positionX = "right";
+	    positionY = "top";
+	    layer = "overlay";
+	    control-center-layer = "top";
+	    layer-shell = true;
+	    cssPriority = "application";
+	    control-center-margin-top = 0;
+	    control-center-margin-bottom = 0;
+	    control-center-margin-right = 0;
+	    control-center-margin-left = 0;
+	    notification-2fa-action = true;
+	    notification-inline-replies = false;
+	    notification-icon-size = 64;
+	    notification-body-image-height = 100;
+	    notification-body-image-width = 200;
+	};
+  };
+
   wayland.systemd.target = "hyprland-session.target";
   wayland.windowManager.hyprland = {
   	enable = true;

0f76e760dc2a4411e5b47655b4a05023918623fc
Aiden <alexismonroy17@proton.me>
2025-06-08 21:49:40 +0200

    New Version Sun Jun  8 09:49:40 PM CEST 2025

--- nixos/home.nix
+++ nixos/home.nix
@@ -41,14 +41,14 @@
   services.swaync = {
     enable = true;
     settings = {
-	    positionX = "right";
+	    positionX = "left";
 	    positionY = "top";
 	    layer = "overlay";
 	    control-center-layer = "top";
 	    layer-shell = true;
 	    cssPriority = "application";
-	    control-center-margin-top = 100;
-	    control-center-margin-bottom = 1000;
+	    control-center-margin-top = 0;
+	    control-center-margin-bottom = 0;
 	    control-center-margin-right = 0;
 	    control-center-margin-left = 0;
 	    notification-2fa-action = true;

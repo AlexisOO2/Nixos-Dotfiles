50fca625942e007436c6be2967e2eee9117468a4
Aiden <alexismonroy17@proton.me>
2025-06-08 21:13:57 +0200

    New Version Sun Jun  8 09:13:57 PM CEST 2025

--- nixos/waybar.nix
+++ nixos/waybar.nix
@@ -40,8 +40,9 @@
     cava= {
         framerate = 30;
         autosens = 1;
-        bars= 14;
-        lower_cutoff_freq= 10;
+        sensetivity = 100;
+	bars= 14;
+        lower_cutoff_freq= 50;
         higher_cutoff_freq= 10000;
         method= "pulse";
         source= "auto";

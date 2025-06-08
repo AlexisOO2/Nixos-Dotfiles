35c9114b65133ca95f28de6c3d4e13481ab02703
Aiden <alexismonroy17@proton.me>
2025-06-08 21:50:04 +0200

    New Version Sun Jun  8 09:50:04 PM CEST 2025

--- nixos/home.nix
+++ nixos/home.nix
@@ -41,7 +41,7 @@
   services.swaync = {
     enable = true;
     settings = {
-	    positionX = "left";
+	    positionX = "right";
 	    positionY = "top";
 	    layer = "overlay";
 	    control-center-layer = "top";

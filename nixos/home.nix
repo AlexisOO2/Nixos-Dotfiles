e1cc90d79792bad41db8e7b4f6256d526bba7d7b
Aiden <alexismonroy17@proton.me>
2025-06-09 11:23:24 +0200

    New Version Mon Jun  9 11:23:24 AM CEST 2025

--- nixos/home.nix
+++ nixos/home.nix
@@ -99,7 +99,7 @@
 		    border_size = 2;
 
 		    # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
-		    "col.active_border" = "rgba(ffffffff) 90deg";
+		    "col.active_border" = "rgba(cc241dff) 90deg";
 		    "col.inactive_border" = "rgba(595959aa)";
 
 		    # Set to true enable resizing windows by clicking and dragging on borders and gaps

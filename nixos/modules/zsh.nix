654184300c6c0c38361517156ebf2fa2ce70f19c
Aiden <alexismonroy17@proton.me>
2025-06-13 22:16:03 +0200

    New Version Fri Jun 13 10:16:03 PM CEST 2025

--- nixos/modules/zsh.nix
+++ nixos/modules/zsh.nix
@@ -18,7 +18,9 @@
       cat = "bat";
       nrs = ''
       sudo nixos-rebuild switch &&
-      sudo cp -r /etc/nixos/ ~/nixos-dotfiles &&
+      sudo cp -r /etc/nixos/packages.nix ~/nixos-dotfiles &&
+      sudo cp -r /etc/nixos/home.nix ~/nixos-dotfiles &&
+      sudo cp -r /etc/nixos/modules ~/nixos-dotfiles &&
       cd ~/nixos-dotfiles
       git add .
       git commit -m "New Version $(date)"

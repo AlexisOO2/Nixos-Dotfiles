22b98b7bfcad4ea782ab6dac9ca88476459018f1
Aiden <alexismonroy17@proton.me>
2025-06-13 22:04:10 +0200

    New Version Fri Jun 13 10:04:10 PM CEST 2025

--- nixos/modules/alacritty.nix
+++ nixos/modules/alacritty.nix
@@ -4,10 +4,6 @@ programs.alacritty = {
     enable = true;
     settings = {
       window.opacity = 0.9;
-      font.normal = {
-        family = "0xProto";
-        style = "Regular";
-      };
       font.size = 12;
     };
   };

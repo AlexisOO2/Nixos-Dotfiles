e9514b6906e41e956a17a7334f8c899ab6d9dcc0
Aiden <alexismonroy17@proton.me>
2025-06-21 16:12:39 +0200

    New Version Sat Jun 21 04:12:39 PM CEST 2025

--- nixos/home.nix
+++ nixos/home.nix
@@ -8,7 +8,6 @@
   home.homeDirectory = "/home/aiden";
   home.stateVersion = "25.05"; 
 
-home.packages = [ inputs.ags.packages.${pkgs.system}.io ]; 
 home.pointerCursor = 
     let 
       getFrom = url: hash: name: {

05d6d4326aa2a1bbe017a11f3a55fd384b3ce4d0
Aiden <alexismonroy17@proton.me>
2025-06-21 18:56:40 +0200

    New Version Sat Jun 21 06:56:40 PM CEST 2025

---
+++ nixos/flake.nix
@@ -0,0 +1,34 @@
+# flake.nix
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs"; 
+    };
+    hyprpanel = {
+      url = "github:Jas-SinghFSU/HyprPanel";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      overlays = inputs.hyprpanel.overlay;
+    in
+    {
+      nixosConfigurations = {
+        nixos-aiden = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            ./configuration.nix
+            { nixpkgs.overlays = [ overlays ]; }
+          ];
+        };
+      };
+    };
+}
